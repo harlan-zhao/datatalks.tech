e66ad8c46f0bf0d3619f664df1a2d4424859e1ec
zhaohehe <zhaohehe520@gmail.com>
2019-07-12 17:40:52 -0400

    finished logic

--- datavisual/autochart/views.py
+++ datavisual/autochart/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
+import os
 
 # Create your views here.
 def upload(request):
+	content ={'warning':'Pleas Upload Only CSV File'}
 	if request.method == 'POST' and request.FILES:
 		folder = 'files'
 		my_file = request.FILES['document']
 		fs = FileSystemStorage(location=folder)
 		filename = fs.save(my_file.name, my_file)
 		file_url = fs.url(filename)
-		return render(request, 'upload.html', {'file_url': file_url})
-	content ={'warning':'Pleas Upload Only CSV File'}
+		return render(request, 'upload.html', content)
 	return render(request,"upload.html",content)
 
 
 def analyze(request):
-	if request.FILES:
-		print(1)
+	if len(os.listdir('./files') ) == 0:
+		content ={'warning':'wrong file'}
+		# return render(request,"upload.html",content)
+		return redirect("../upload/")
+
+
 	return render(request,"visualization.html")
