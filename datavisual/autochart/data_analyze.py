a7506bfdd96d97991c066b5d55dc74d8af510a9f
datatalks <zhaohehe520@gmail.com>
2019-08-18 02:55:56 +0000

    update gitignore

--- datavisual/autochart/data_analyze.py
+++ datavisual/autochart/data_analyze.py
@@ -13,31 +13,19 @@ from bokeh.resources import CDN
 def analyze(file_id,file,ins1,ins2):
     path = '/home/harlanzhao/files/'+file_id+'/'+file
     df = pd.read_csv(path)
+    xaxis,yaxis = df[ins1],df[ins2]
     if ins1 == ins2:
-<<<<<<< HEAD
-        pass
-    else:
-        df = pd.read_csv(path)
-        xaxis,yaxis = df[ins1],df[ins2]
-        p = figure(plot_width=1020,plot_height=710, title=f"{ins1}/{ins2}",
-            toolbar_location="right", tools="pan,wheel_zoom,box_zoom,reset, hover, save,tap, crosshair")
-        p.add_tools(LassoSelectTool())
-        p.add_tools(WheelZoomTool())
-        p.circle(xaxis, yaxis)
-    script, div = components(p)
-=======
-		p = figure(plot_width=1020,plot_height=710, title=f"{ins1}/{ins2}",x_axis_label = f'{ins1}', 
+        p = figure(plot_width=1020,plot_height=710, title=f"{ins1}/{ins2}",x_axis_label = f'{ins1}',
            y_axis_label = f'{ins2}',toolbar_location="right", tools="pan,wheel_zoom,box_zoom,reset, hover, save,tap, crosshair")
-		p.vbar(x=[i+1 for i in range(len(df[ins1]))], width=0.5, bottom=0,
-       	top=df[ins1], color="firebrick")
+        p.vbar(x=[i+1 for i in range(len(df[ins1]))], width=0.5, bottom=0,
+           top=df[ins1], color="firebrick")
 
-	else:
-		p = figure(plot_width=1020,plot_height=710, title=f"{ins1}/{ins2}",x_axis_label = f'{ins1}', 
+    else:
+        p = figure(plot_width=1020,plot_height=710, title=f"{ins1}/{ins2}",x_axis_label = f'{ins1}',
            y_axis_label = f'{ins2}',toolbar_location="right", tools="pan,wheel_zoom,box_zoom,reset, hover, save,tap, crosshair")
-		p.circle(xaxis, yaxis)
-	p.add_tools(LassoSelectTool())
-	p.add_tools(WheelZoomTool())
->>>>>>> 88a9bfac76d8d9563850acc71647b217b3438d4e
+        p.circle(xaxis, yaxis)
+    p.add_tools(LassoSelectTool())
+    p.add_tools(WheelZoomTool())
     script, div = components(p)
     return script, div
 
